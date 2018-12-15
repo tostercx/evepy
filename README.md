44b6d6c2eb1c6decc33f71fb13318f0cebe7c76a
toster <tostercx@gmail.com>
2018-12-15 16:28:26 +0200

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# evepy
+
+Standalone eve python launcher
+
+You might have to mess with getEvePath if not on steam / win x64... or just hardcode it.
+
+
+### usage:
+
+execute a file:
+
+    evepy file.py
+
+interactive mode:
+
+    evepy
---
+++ evepy.cpp
@@ -0,0 +1,87 @@
+#include <windows.h>
+#include <stdio.h>
+
+#pragma comment (lib, "advapi32")
+
+//typedef int (__stdcall *PYRUN_SIMPLEFILE)(FILE *fp, const char *str);
+typedef int (__stdcall *PYRUN_SIMPLESTRING)(const char *str);
+typedef void (__stdcall *PY_INITIALIZE)();
+
+//PYRUN_SIMPLEFILE PyRun_SimpleFile;
+PYRUN_SIMPLESTRING PyRun_SimpleString;
+PY_INITIALIZE Py_Initialize;
+
+HMODULE hModule;
+
+BOOL getEvePath(char *path, DWORD len)
+{
+      HKEY hKey;
+      LONG res;
+      DWORD type = REG_SZ;
+      
+      // for steamed versoin on win64
+      res=RegOpenKeyEx(HKEY_LOCAL_MACHINE,"Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Steam App 8500",0,KEY_ALL_ACCESS | KEY_WOW64_64KEY,&hKey);
+      if(res!=ERROR_SUCCESS) return FALSE;
+      
+      res=RegQueryValueEx(hKey,"InstallLocation", NULL, &type, (LPBYTE)path, &len);
+      RegCloseKey(hKey);
+      
+      if(res!=ERROR_SUCCESS) return FALSE;
+      return TRUE;
+}
+
+int main(int argc, char **argv)
+{
+    char buf[8192];
+    char path[MAX_PATH];
+    
+    // get EVE path
+    if(getEvePath(path, MAX_PATH))
+    {
+        strcat(path, "\\SharedCache\\tq");
+        printf("EVE dir: %s\n", path);
+    }
+    else
+    {
+        printf("Failed to find eve path :(\n");
+        return 1;
+    }
+    
+    // MUST be dona before loading the dll
+    sprintf(buf, "%s\\code.ccp;%s\\bin", path, path);
+    _putenv_s("PYTHONPATH", buf);
+    
+    // get python
+    sprintf(buf, "%s\\bin\\python27.dll", path);
+    hModule = LoadLibrary(buf);
+    //PyRun_SimpleFile    = (PYRUN_SIMPLEFILE)    GetProcAddress(hModule, "PyRun_SimpleFile");
+    PyRun_SimpleString  = (PYRUN_SIMPLESTRING)  GetProcAddress(hModule, "PyRun_SimpleString");
+    Py_Initialize       = (PY_INITIALIZE)       GetProcAddress(hModule, "Py_Initialize");
+    
+    // display debug info
+    //printf("PyRun_SimpleFile:   %X\n", (unsigned int)PyRun_SimpleFile);
+    printf("PyRun_SimpleString: %X\n", (unsigned int)PyRun_SimpleString);
+    printf("Py_Initialize:      %X\n", (unsigned int)Py_Initialize);
+    printf("\n");
+    
+    Py_Initialize();
+    
+    if (argc > 1)
+    {
+        FILE *fp = fopen(argv[1], "r");
+        
+        // SimpleFile emulator
+        int len = fread(buf, 1, 8192, fp);
+        buf[len] = 0;
+        fclose(fp);
+        
+        PyRun_SimpleString(buf);
+    }
+    else
+    {
+        PyRun_SimpleString("import sys, code");
+        PyRun_SimpleString("code.interact()");
+    }
+    
+    return 0;
+}
