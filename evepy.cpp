2e6efecbfb3d2f0ba9403f0194dc0b845764808e
toster <tostercx@gmail.com>
2019-11-25 01:32:16 +0200

    fix new path stuff

--- evepy.cpp
+++ evepy.cpp
@@ -10,6 +10,7 @@ typedef void (__stdcall *PY_INITIALIZE)();
 //PYRUN_SIMPLEFILE PyRun_SimpleFile;
 PYRUN_SIMPLESTRING PyRun_SimpleString;
 PY_INITIALIZE Py_Initialize;
+wchar_t **Py_GetPathWData;
 
 HMODULE hModule;
 
@@ -33,6 +34,7 @@ BOOL getEvePath(char *path, DWORD len)
 int main(int argc, char **argv)
 {
     char buf[100000];
+    wchar_t buf2[10000];
     char path[MAX_PATH];
     
     // get EVE path
@@ -47,22 +49,27 @@ int main(int argc, char **argv)
         return 1;
     }
     
-    // MUST be dona before loading the dll
-    sprintf(buf, "%s\\code.ccp;%s\\bin64", path, path);
-    _putenv_s("PYTHONPATH", buf);
-    
     // get python
     sprintf(buf, "%s\\bin64\\python27.dll", path);
     hModule = LoadLibrary(buf);
     //PyRun_SimpleFile    = (PYRUN_SIMPLEFILE)    GetProcAddress(hModule, "PyRun_SimpleFile");
     PyRun_SimpleString  = (PYRUN_SIMPLESTRING)  GetProcAddress(hModule, "PyRun_SimpleString");
     Py_Initialize       = (PY_INITIALIZE)       GetProcAddress(hModule, "Py_Initialize");
+    Py_GetPathWData     = (wchar_t**)           GetProcAddress(hModule, "Py_GetPathWData");
+    
+    // MUST be done before init
+    swprintf(buf2, 10000, L"%hs\\code.ccp;%hs\\bin64", path, path);
+    wprintf(L"%s\n", buf2);
+    *Py_GetPathWData = buf2;
     
     // display debug info
     //printf("PyRun_SimpleFile:   %X\n", (unsigned int)PyRun_SimpleFile);
-    printf("PyRun_SimpleString: %X\n", (unsigned int)PyRun_SimpleString);
-    printf("Py_Initialize:      %X\n", (unsigned int)Py_Initialize);
+    printf("Py_GetPathWData:         %p\n", (void *)Py_GetPathWData);
+    wprintf(L"Py_GetPathWData:         %s\n", *Py_GetPathWData);
+    printf("PyRun_SimpleString: %p\n", (void *)PyRun_SimpleString);
+    printf("Py_Initialize:      %p\n", (void *)Py_Initialize);
     printf("\n");
+    
     
     Py_Initialize();
     
